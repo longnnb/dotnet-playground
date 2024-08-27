39f145ba71f01305960c846dc56a3e09681d4b4e
Long Nguyen <longnnb@hotmail.com>
2024-08-27 17:26:44 +0700

    wip

---
+++ src/CSharpFeatures/OutVariables.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace CSharpFeatures;
+public class OutVariables
+{
+    public void TryParseInt()
+    {
+        string input = "123";
+        // try parse to int and assign to result
+        if (int.TryParse(input, out int result))
+        {
+            WriteLine(result);
+        }
+
+        // what if the parse fails?
+        int.TryParse("abc", out var i);
+        WriteLine($"i = {i}"); // default value
+    }
+
+    public void TryParseDateTime()
+    {
+        DateTime dt; // struct
+        if (DateTime.TryParse("01/01/2017", out dt))
+        {
+            WriteLine($"Old-fashioned parse: {dt}");
+        }
+
+        // variable declaration is an expression, not a statement
+        if (DateTime.TryParse("02/02/2016", out /*DateTime*/ var dt2))
+        {
+            WriteLine($"New parse: {dt2}");
+        }
+
+        // the scope of dt2 extends outside the if block
+        WriteLine($"I can use dt2 here: {dt2}");
+    }
+}
---
+++ src/CSharpFeatures/PatternMatching.cs
@@ -0,0 +1,99 @@
+﻿using static System.Console;
+
+namespace CSharpFeatures;
+public class PatternMatching
+{
+    public void GetSumOfIntValue()
+    {
+        object[] data = { 1, "two", 3, "four", 5 };
+        int sum = 0;
+        foreach (var item in data)
+        {
+            // check if item is int and assign to value  
+            if (item is int intValue)
+            {
+                sum += intValue;
+            }
+        }
+        WriteLine($"Sum of integers: {sum}");
+
+        // switch statement
+        foreach (var item in data)
+        {
+            switch (item)
+            {
+                case int intValue: // check and assign
+                    sum += intValue;
+                    break;
+                case string textValue: // check and assign
+                    WriteLine($"Text: {textValue}");
+                    break;
+            }
+        }
+        WriteLine($"Sum of integers: {sum}");
+    }
+
+    public void DisplayShape(Shape shape)
+    {
+        // old way
+        if (shape is Rectangle)
+        {
+            var rc = (Rectangle)shape;
+
+        }
+        else if (shape is Circle)
+        {
+            // ...
+        }
+
+        // old way
+        var rect = shape as Rectangle;
+        if (rect != null) // nonnull
+        {
+            //...
+        }
+
+
+        // new way
+        if (shape is Rectangle r)
+        {
+            // use r
+        }
+
+        // can also do the invserse
+        if (!(shape is Circle cc))
+        {
+            // not a circle!
+        }
+
+        switch (shape)
+        {
+            case Circle c:
+                // use c
+                break;
+            case Rectangle sq when sq.Width == sq.Height: // assign with condition
+                // square!
+                break;
+            case Rectangle rr:
+                // use rr
+                break;
+        }
+    }
+
+}
+
+
+public class Shape
+{
+
+}
+
+public class Rectangle : Shape
+{
+    public int Width, Height;
+}
+
+public class Circle : Shape
+{
+    public int Diameter;
+}
